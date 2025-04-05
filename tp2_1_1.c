8342576b05df79b54788d54003e2f5223bb83a31
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-04-05 12:02:38 -0300

    codigo completado

---
+++ tp2_1_1.c
@@ -0,0 +1,13 @@
+// codigo a completar
+#define N 20
+int i;
+double vt[N];
+int main()
+{
+    for (i = 0; i < N; i++)
+    {
+        vt[i] = 1 + rand() % 100;
+        printf("%f", vt[i]);
+    }
+    return 0;
+}
