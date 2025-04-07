183828bc0f5c8a6cf19715bbe326173aa6543896
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-04-07 18:35:00 -0300

    creacion del procedimiento listarPCS

--- aplicacion.c
+++ aplicacion.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #define N 5
+
 struct compu
 {
   int velocidad;        // Velocidad de procesamiento en GHz (valor entre 1 y 3)
@@ -10,11 +11,13 @@ struct compu
   char *tipo_cpu;       // Tipo de procesador (apuntador a cadena de caracteres)
 };
 
+void listarPCs(struct compu pcs[], int cantidad);
+
 int main()
 {
   srand(time(NULL));
 
-  struct compu pc[5];
+  struct compu pcs[N];
 
   char tipos[6][20] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
@@ -22,25 +25,31 @@ int main()
 
   for (int i = 0; i < 6; i++)
   {
-    tipo_cpu[i] = tipos[i]; // cada puntero apunta a una cadena de tipos
+    tipo_cpu[i] = tipos[i]; 
   }
 
   for (int i = 0; i < N; i++)
   {
-    pc[i].velocidad = 1 + rand() % 3;
-    pc[i].anio = 2015 + rand() % 10;
-    pc[i].cantidad_nucleos = 1 + rand() % 8;
+    pcs[i].velocidad = 1 + rand() % 3;
+    pcs[i].anio = 2015 + rand() % 10;
+    pcs[i].cantidad_nucleos = 1 + rand() % 8;
 
     int indice = rand() % 6;
-    pc[i].tipo_cpu = tipo_cpu[indice];
+    pcs[i].tipo_cpu = tipo_cpu[indice];
   }
 
-  for (int i = 0; i < N; i++)
+  listarPCs(pcs, N);
+}
+
+void listarPCs(struct compu pcs[], int cantidad)
+{
+
+  for (int i = 0; i < cantidad; i++)
   {
     printf("\nDatos de la computadora %d:\n", i + 1);
-    printf("Velocidad: %d GHz, ", pc[i].velocidad);
-    printf("Anio: %d, ", pc[i].anio);
-    printf("Cantidad de nucleos: %d, ", pc[i].cantidad_nucleos);
-    printf("Tipo de CPU: %s\n", pc[i].tipo_cpu);
+    printf("Velocidad: %d GHz, ", pcs[i].velocidad);
+    printf("Anio: %d, ", pcs[i].anio);
+    printf("Cantidad de nucleos: %d, ", pcs[i].cantidad_nucleos);
+    printf("Tipo de CPU: %s\n", pcs[i].tipo_cpu);
   }
 }
